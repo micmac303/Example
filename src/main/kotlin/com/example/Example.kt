6a8c2b134a6df5c97f8980c645e94693854b7b2d
michaelmccarthy <micmac303@gmail.com>
2024-12-07 00:32:47 +0000

    Removed unnecessary variable abstraction

--- src/main/kotlin/com/example/Example.kt
+++ src/main/kotlin/com/example/Example.kt
@@ -1,16 +1,13 @@
 package com.example
 
-import org.http4k.core.HttpHandler
 import org.http4k.core.then
 import org.http4k.filter.DebuggingFilters.PrintRequest
 import org.http4k.server.SunHttp
 import org.http4k.server.asServer
 
-val app: HttpHandler = ExampleApi.routes
-
 fun main() {
 
     println("Server starting...")
-    val server = PrintRequest().then(app).asServer(SunHttp(9000)).start()
+    val server = PrintRequest().then(ExampleApi.routes).asServer(SunHttp(9000)).start()
     println("Server started on " + server.port())
 }
