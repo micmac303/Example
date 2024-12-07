f3c60935504ef6d4e37b7610bc2e8aa706fe9abb
michaelmccarthy <micmac303@gmail.com>
2024-12-07 01:14:03 +0000

    Test fixed

--- src/test/kotlin/com/example/ExampleTest.kt
+++ src/test/kotlin/com/example/ExampleTest.kt
@@ -11,7 +11,7 @@ class ExampleTest {
 
     @Test
     fun `Ping test`() {
-        assertEquals(Response(OK).body("pong"), app(Request(GET, "/ping")))
+        assertEquals(Response(OK).body("pong"), ExampleApi.routes(Request(GET, "/ping")))
     }
 
 }
